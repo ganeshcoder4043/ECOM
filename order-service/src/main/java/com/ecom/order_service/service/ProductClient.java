0b4492ab487167ebdc4ee8ec8edff52a3a48c9db
ganeshcoder4043 <ganesha4043@gmail.com>
2026-09-16 02:46:23 +0530

    service discovery complete

--- order-service/src/main/java/com/ecom/order_service/service/ProductClient.java
+++ order-service/src/main/java/com/ecom/order_service/service/ProductClient.java
@@ -1,10 +1,12 @@
 package com.ecom.order_service.service;
 
 import com.ecom.order_service.dto.ProductResponseDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
+@Slf4j
 public class ProductClient {
 
     private final RestTemplate restTemplate;
@@ -13,15 +15,31 @@ public class ProductClient {
         this.restTemplate = restTemplate;
     }
 
-    public ProductResponseDTO getProductName(String productId){
-        String url = "http://localhost:6000/ecommerce/products/" + productId;
+    /*public ProductResponseDTO getProductName(String productId){
+//        String url = "http://localhost:6000/ecommerce/products/" + productId;
+        String url = "http://product-service/ecommerce/products/" + productId;
         return restTemplate.getForObject(url, ProductResponseDTO.class);
     }
 
     public void updateStock(String productId, int quantity){
 //        String url = "http://localhost:6000/ecommerce/products/" + productId + "/stock?stockQuantity=" + quantity;
-        String url = "http://localhost:6000/ecommerce/products/" + productId + "/stock?stockQuantity=" + quantity;
+//        String url = "http://localhost:6000/ecommerce/products/" + productId + "/stock?stockQuantity=" + quantity;
+        String url = "http://product-service/ecommerce/products/" + productId + "/stock?stockQuantity=" + quantity;
 //        restTemplate.patchForObject(url, null, Void.class);
+        restTemplate.put(url, null);
+    }*/
+
+
+    public ProductResponseDTO getProductName(String productId) {
+        // ✅ Service name (Eureka registered name)
+        String url = "http://product-service/ecommerce/products/" + productId;
+        log.info("Calling Product Service: {}", url);
+        return restTemplate.getForObject(url, ProductResponseDTO.class);
+    }
+
+    public void updateStock(String productId, int quantity) {
+        String url = "http://product-service/ecommerce/products/" + productId + "/stock?stockQuantity=" + quantity;
+        log.info("Updating stock: {}", url);
         restTemplate.put(url, null);
     }
 }
